537b574af4ec556b3458fda9a92a8ad95a3135e3
stefanliemawan <stefanliemawan@gmail.com>
2022-11-13 16:36:50 +0000

    added scaler

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 import pickle
+import joblib
 
 app = Flask(__name__)
 
 model = pickle.load(open("model.pickle", "rb"))
+scaler = joblib.load("scaler.gz")
 
 
 @app.route("/")
@@ -15,4 +17,6 @@ def home():
 def predict():
     n_month = request.args.get("month")
     prediction = model.predict(1185, 1185 + int(n_month) - 1)
+    prediction = scaler.inverse_transform(prediction.reshape(-1,1))
+    prediction = prediction.reshape(-1,)
     return jsonify(prediction.tolist())
